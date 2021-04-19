4003241e818cbaabf5e1194eceb0bd0576cc8728
supreeth23797 <37664479+supreeth23797@users.noreply.github.com>
2021-04-19 09:44:44 +0530

    Add files via upload

--- src/main/java/com/news/newsapp/Utils.java
+++ src/main/java/com/news/newsapp/Utils.java
@@ -2,7 +2,9 @@ package com.news.newsapp;
 
 public class Utils {
 
-    public static final String API_KEY = "bc6de81471df423a9de39c0a68d16d30";
+    // Get your key from newsapi.org
+    public static final String API_KEY = " YOUR NEWSAPI.ORG KEY";
+    public static final String COUNTRY = "in";
     //For Pagination
     public static final int PAGE_SIZE = 10;
 }
